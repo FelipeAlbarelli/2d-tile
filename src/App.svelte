e5b0bcf378901f4c3aa6113304f717f6d3048069
FelipeAlbarelli <felipesalbarelli@gmail.com>
2024-04-05 13:41:22 -0300

    start tile comp

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
+  import colored from './assets/colored.png'
   import Counter from './lib/Counter.svelte';
    import { Stage, Layer, Rect , Image } from 'svelte-konva';
+    import Tile from './lib/Tile.svelte';
 </script>
 
 <div class="main">
   <div class="side-bar">
-    <img   />
+    <Stage config={{ width: 100, height: window.innerHeight - 40 }} > 
+      <Layer
+      config={{imageSmoothingEnabled: false }}
+      >
+        <Tile />
+
+      </Layer>
+    </Stage>
   </div>
   <div class="stage">
     <Stage config={{ width: 500, height: 300 }} />
@@ -18,9 +27,9 @@
 <style>
   img  {
     width : 50px;
+    image-resolution: 2;
     height : 50px;
-    background: url(assets/colored.png) ;
-    object-fill: 
+    image-rendering: pixelated;
   }
 
   .main {
@@ -34,7 +43,7 @@
   .side-bar {
     border: 2px solid white;
     height: 100%;
-    width: 100px;
+    /* width: 100px; */
   }
 
 </style>
---
+++ src/lib/Tile.svelte
@@ -0,0 +1,33 @@
+<script  lang="ts" >
+    import { onMount, tick } from "svelte";
+    import { Stage, Layer , Image, type KonvaMouseEvent } from "svelte-konva";
+    import asset from '../assets/colored.png'
+    import type { KonvaEventObject } from "konva/lib/Node";
+    import type { Image as KonvaImage } from "konva/lib/shapes/Image";
+
+    let image : HTMLImageElement | undefined = undefined;
+    let handle : KonvaImage; 
+    onMount(() => {
+        const img = document.createElement("img");
+        img.src = asset;
+        img.onload = () => {
+            image = img;
+        };
+    });
+    function handleCircleClick(e : KonvaMouseEvent) {
+        console.log(e);
+        handle.crop()
+        
+    }
+
+</script>
+
+<Image 
+    on:click={handleCircleClick}
+    bind:handle={handle}
+    config={{ image , x: 0 , y: 0 , width: 16 * 5, height: 16 * 5 , offsetX: 0, offsetY : 0, 
+        fillPatternOffset : {x: 0, y: 0},
+        fillPatternScale : {x: 16 * 5, y : 16 * 5},
+        fill : 'fill'
+         }}     
+    />

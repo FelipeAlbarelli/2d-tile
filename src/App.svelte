8b3f40a1757428fd4629c8ea8537088338746bf2
FelipeAlbarelli <39565688+FelipeAlbarelli@users.noreply.github.com>
2024-04-05 12:59:02 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte';
+   import { Stage, Layer, Rect , Image } from 'svelte-konva';
+</script>
+
+<div class="main">
+  <div class="side-bar">
+    <img   />
+  </div>
+  <div class="stage">
+    <Stage config={{ width: 500, height: 300 }} />
+  </div>
+
+</div>
+
+<style>
+  img  {
+    width : 50px;
+    height : 50px;
+    background: url(assets/colored.png) ;
+    object-fill: 
+  }
+
+  .main {
+    height: 100vh;
+    width: 100vw;
+    padding: 12px;
+    display: flex;
+    
+  }
+
+  .side-bar {
+    border: 2px solid white;
+    height: 100%;
+    width: 100px;
+  }
+
+</style>
